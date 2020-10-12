a24df0b1456d41ab41970cf034e2480bfdd5b915
zhaopengfei <1027926875@qq.com>
2020-10-12 21:18:34 +0800

    Language Basic

---
+++ src/main/java/com/github/java/languagebasic/ArrayDemo.java
@@ -0,0 +1,50 @@
+package com.github.java.languagebasic;
+
+/**
+ * @author pengfei.zhao
+ * @date 2020/10/12 20:40
+ */
+public class ArrayDemo {
+    public static void main(String[] args) {
+        // declares an array of integers
+        int[] anArray;
+
+        // allocates memory for 10 integers
+        anArray = new int[10];
+
+        // initialize first element
+        anArray[0] = 100;
+        // initialize second element
+        anArray[1] = 200;
+        // and so forth
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+
+        System.out.println("Element at index 0: "
+                + anArray[0]);
+        System.out.println("Element at index 1: "
+                + anArray[1]);
+        System.out.println("Element at index 2: "
+                + anArray[2]);
+        System.out.println("Element at index 3: "
+                + anArray[3]);
+        System.out.println("Element at index 4: "
+                + anArray[4]);
+        System.out.println("Element at index 5: "
+                + anArray[5]);
+        System.out.println("Element at index 6: "
+                + anArray[6]);
+        System.out.println("Element at index 7: "
+                + anArray[7]);
+        System.out.println("Element at index 8: "
+                + anArray[8]);
+        System.out.println("Element at index 9: "
+                + anArray[9]);
+    }
+}
---
+++ src/main/java/com/github/java/languagebasic/MultiDimArrayDemo.java
@@ -0,0 +1,18 @@
+package com.github.java.languagebasic;
+
+/**
+ * @author pengfei.zhao
+ * @date 2020/10/12 20:48
+ */
+public class MultiDimArrayDemo {
+    public static void main(String[] args) {
+        String[][] names = {
+                {"Mr. ", "Mrs. ", "Ms. "},
+                {"Smith", "Jones"}
+        };
+        // Mr. Smith
+        System.out.println(names[0][0] + names[1][0]);
+        // Ms. Jones
+        System.out.println(names[0][2] + names[1][1]);
+    }
+}
